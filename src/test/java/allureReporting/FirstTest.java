9c866cd04fea70dee51c94df503e434448f1e649
Shetty <narasayyashetty497@gmail.com>
2025-06-24 12:52:55 +0530

    First commit

---
+++ src/test/java/allureReporting/FirstTest.java
@@ -0,0 +1,18 @@
+package allureReporting;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	
+	@Test
+	public void testCase01() {
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void testcase02() {
+		System.out.println("dfghjk");
+	}
+
+}
